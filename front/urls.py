f00067daecf4897029dee0fd624e5ab2e2901a52
m.sharapov <m.sharapov@NB0487.local>
2023-07-06 15:52:40 +0300

    first commit

---
+++ front/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about'),
+    path('login', views.signin, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.signout, name='logout'),
+    path('help', views.help_view, name='help'),
+    path('account', views.account, name='account'),
+    path('trade', views.trade, name='trade'),
+    path('shop', views.shop, name='shop'),
+    path('note', views.note, name='note'),
+    path('chats', views.chats, name='chats'),
+    path('favorites', views.favourites, name='favorites'),
+    path('my_products', views.my_products, name='my_products'),
+    path('my_orders', views.my_orders, name='my_orders'),
+    path('news', views.news, name='news'),
+    path('product', views.product, name='product'),
+    path('yields', views.yields , name='yields'),
+    path('order', views.order , name='order'),
+    path('profile', views.profile, name='profile'),
+    path('favorite', views.favorite, name='favorite'),
+    path('favorite_for_yields', views.favorite_for_yields, name='favorite_for_yields'),
+    path('chat', views.chat, name='chat'), 
+    path('product-update', views.productUpdateView , name='product-update'),
+    path('product-delete', views.productDeleteView , name='product-delete'),
+    path('user-delete', views.userDeleteView , name='user-delete'),
+    path('favorite-delete', views.favoriteDeleteView , name='favorite-delete'),
+    path('order-update', views.orderUpdateView , name='order-update'),
+    path('order-delete', views.orderDeleteView, name='order-delete')
+]
---
+++ front/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404, HttpRequest, HttpResponse
+
+def index(request):
+    context = {}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'main/about.html', context)
+
+def signin(request):
+    context = {}
+    return render(request, 'main/login.html', context)
+
+def register(request):
+    context = {}
+    return render(request, 'main/register.html', context)
+
+def signout(request):
+    context = {}
+    return render(request, 'main/logout.html', context)
+
+def help_view(request):
+    context = {}
+    return render(request, 'main/help.html', context)
+
+def account(request):
+    context = {}
+    return render(request, 'main/account.html', context)
+
+def trade(request):
+    context = {}
+    return render(request, 'main/trade.html', context)
+
+def shop(request):
+    context = {}
+    return render(request, 'main/shop.html', context)
+
+def chats(request):
+    context = {}
+    return render(request, 'main/chats.html', context)
+
+def note(request):
+    context = {}
+    return render(request, 'main/note.html', context)
+
+def favourites(request):
+    context = {}
+    return render(request, 'main/favorites.html', context)
+
+def my_products(request):
+    context = {}
+    return render(request, 'main/my_products.html', context)
+
+def my_orders(request):
+    context = {}
+    return render(request, 'main/my_orders.html', context)
+
+def news(request):
+    context = {}
+    return render(request, 'main/news.html', context)
+
+def product(request):
+    context = {}
+    return render(request, 'main/product.html', context)
+
+def yields(request):
+    context = {}
+    return render(request, 'main/yields.html', context)
+
+def order(request):
+    context = {}
+    return render(request, 'main/order.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'main/profile.html', context)
+
+def favorite(request):
+    context = {}
+    return render(request, 'main/favorite.html', context)
+
+def favorite_for_yields(request):
+    context = {}
+    return render(request, 'main/favorite_for_yields.html', context)
+
+def chat(request):
+    context = {}
+    return render(request, 'main/chat.html', context)
+
+def productUpdateView(request):
+    context = {}
+    return render(request, 'main/product-update.html', context)
+
+def productDeleteView(request):
+    context = {}
+    return render(request, 'main/product-delete.html', context)
+
+def userDeleteView(request):
+    context = {}
+    return render(request, 'main/user-delete.html', context)
+
+def favoriteDeleteView(request):
+    context = {}
+    return render(request, 'main/favorite-delete.html', context)
+
+def orderUpdateView(request):
+    context = {}
+    return render(request, 'main/order-update.html', context)
+
+def orderDeleteView(request):
+    context = {}
+    return render(request, 'main/order-delete.html', context)
+
